2cdd95fdf65e381f098724147e716fb9aafc9180
satyam kumar mishra <satyamkmishraa@gmail.com>
2025-03-28 18:13:31 +0530

    Updated project with new changes

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -8,47 +8,71 @@ const projects = [
     description: "A Quora-like post-sharing platform using REST API.",
     tech: ["Node.js", "Express.js", "EJS", "CSS"],
     liveDemo: "https://your-live-demo-link.com",
-    github: "https://github.com/Satyam6201/Quora-Post",
+    github: "https://github.com/Satyam6201/Quora-Post"
   },
   {
     title: "Digital Clock",
     image: "/assets/Digital-clock-App.png",
     description: "A modern digital clock with alarm, themes, and PWA support.",
     tech: ["React.js", "JavaScript", "HTML", "CSS"],
-    liveDemo: "https://your-live-demo-link.com",
-    github: "https://github.com/Satyam6201/Digital-Clock-App",
+    liveDemo: "https://digital-clock-app-12.vercel.app/",
+    github: "https://github.com/Satyam6201/Digital-Clock-App"
   },
   {
     title: "Memory Card Game",
     image: "/assets/memory-card-game.avif",
     description: "A fun memory-matching card game with animations.",
     tech: ["React.js", "JavaScript", "HTML", "CSS"],
-    liveDemo: "https://your-live-demo-link.com",
-    github: "https://github.com/Satyam6201/Memory-Card-Game",
+    liveDemo: "https://memory-card-game-bice-zeta.vercel.app/",
+    github: "https://github.com/Satyam6201/Memory-Card-Game"
   },
   {
     title: "Quiz App",
     image: "/assets/Quiz Game.jpg",
     description: "An interactive quiz app with API-based questions & scoring.",
     tech: ["React.js", "JavaScript", "API", "CSS"],
-    liveDemo: "https://your-live-demo-link.com",
-    github: "https://github.com/Satyam6201/Quiz-App",
+    liveDemo: "https://quiz-app-zeta-rust-62.vercel.app/",
+    github: "https://github.com/Satyam6201/Quiz-App"
   },
   {
     title: "SaaS-Dashboard",
     image: "/assets/SaaS-Dashboard.png",
-    description: "Enhanced SaaS Dashboard with modern UI, animated sidebar, dark mode fixes, and improved charts.",
+    description: "Enhanced SaaS Dashboard with a modern UI, animated sidebar, dark mode fixes.",
     tech: ["React.js", "JavaScript", "HTML", "CSS"],
-    liveDemo: "https://your-live-demo-link.com",
-    github: "https://github.com/Satyam6201/SaaS-Dashboard",
+    liveDemo: "https://saas-dashboard-teal.vercel.app/",
+    github: "https://github.com/Satyam6201/SaaS-Dashboard"
   },
   {
     title: "Weather App",
     image: "/assets/weather-app.jpg",
     description: "A weather forecast app fetching real-time data from an API.",
     tech: ["JavaScript", "API", "HTML", "CSS"],
-    liveDemo: "https://your-live-demo-link.com",
-    github: "https://github.com/Satyam6201/Weather-App",
+    liveDemo: "https://weather-app-seven-ashen-32.vercel.app/",
+    github: "https://github.com/Satyam6201/Weather-App"
+  },
+  {
+    title: "Tic-Tac-Toe-Game",
+    image: "/assets/Tic Tac Toe.jpg",
+    description: "Players take turns marking squares; the first to align three wins.",
+    tech: ["JavaScript", "HTML", "CSS"],
+    liveDemo: "https://tic-tac-toe-game-xi-peach.vercel.app/",
+    github: "https://github.com/Satyam6201/Tic-Tac-Toe-Game"
+  },
+  {
+    title: "2D Brick Breaker Game",
+    image: "/assets/2D Brick Breaker.png",
+    description: "Fun for all ages with increasing excitement as more players join! 🚀🎮.",
+    tech: ["JavaScript", "HTML", "CSS"],
+    liveDemo: "https://2-d-brick-breaker-game.vercel.app/",
+    github: "https://github.com/Satyam6201/2D-Brick-Breaker-Game"
+  },
+  {
+    title: "Whack-a-mole",
+    image: "/assets/Whack a Mole.jpg",
+    description: " players click moles appearing randomly in a grid to score points🎯🎮🚀.",
+    tech: ["JavaScript", "HTML", "CSS"],
+    liveDemo: "https://whack-a-mole-game-one.vercel.app/",
+    github: "https://github.com/Satyam6201/Whack-a-mole-Game",
   },
 ];
 
